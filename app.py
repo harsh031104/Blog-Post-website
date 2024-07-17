ea4eda56d631491edd9ec41680059235a96752ca
0311204 <himansh03112004@gmail.com>
2024-07-17 13:50:53 +0530

    connection string add

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import datetime
 from pymongo import MongoClient
 
 app = Flask(__name__)
-client = MongoClient('localhost', 27017)
+client = MongoClient("mongodb+srv://himansh03112004:harsh2004db@cluster-1.wlgfddo.mongodb.net/")
 app.db = client.microblog
 
 @app.route("/", methods=["GET", "POST"])
